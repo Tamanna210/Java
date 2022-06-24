cf36d9fe81698716ac0e3c4a4880288226a3d1a8
Banuprakash <banuprakashc@yahoo.co.in>
2022-06-24 12:28:16 +0530

    day 5

--- orderapp/src/main/java/com/adobe/prj/OrderappApplication.java
+++ orderapp/src/main/java/com/adobe/prj/OrderappApplication.java
@@ -1,5 +1,6 @@
 package com.adobe.prj;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adobe.prj.entity.Customer;
+import com.adobe.prj.entity.Item;
+import com.adobe.prj.entity.Order;
 import com.adobe.prj.entity.Product;
 import com.adobe.prj.service.OrderService;
 
@@ -28,7 +32,35 @@ public class OrderappApplication implements CommandLineRunner {
 //		getById();
 //		addProductData();
 //		filterByPrice();
-		modifyProduct();
+//		modifyProduct();
+		
+		createOrder();
+	}
+
+	private void createOrder() {
+		Order order = new Order(); // order_date is set, oid is auto_increment
+		Customer c = new Customer();
+		c.setEmail("sam@adobe.com"); // user logins and this data will be in client app
+		
+		
+		Item i1 = new Item();
+		Product p1 = new Product();
+		p1.setId(3);
+		i1.setProduct(p1);
+		i1.setQty(3);
+
+		Item i2 = new Item();
+		Product p2 = new Product();
+		p2.setId(1);
+		i2.setProduct(p2);
+		i2.setQty(1);
+		
+		
+		order.setCustomer(c);
+		order.getItems().add(i1);
+		order.getItems().add(i2);
+		
+		service.placeOrder(order);
 	}
 
 	private void modifyProduct() {
